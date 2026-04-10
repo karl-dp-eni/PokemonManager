8ffdba041ecc5e8a6e4bb41206bbcf0634b5d641
karl-dp-eni <karl.dupuy2025@campus-eni.fr>
2026-04-10 09:42:48 +0200

    Initializing PokemonManager project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {GenerationsComponent} from "./components/generations/generations.component";
+import {PokemonsComponent} from "./components/pokemons/pokemons.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GenerationsComponent, PokemonsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/generations/generations.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generations',
+  standalone: true,
+  imports: [],
+  templateUrl: './generations.component.html',
+  styleUrl: './generations.component.css'
+})
+export class GenerationsComponent {
+
+}
